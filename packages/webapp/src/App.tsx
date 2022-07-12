204548480123e5a9c9d9d0ca2a8e3a10acbac5fb
Quentin WIDLOCHER <quentin.widlocher@erudo.fr>
2022-07-12 12:08:45 +0200

    🔧 Configure shared lib

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -1,46 +1,55 @@
-import { Component, createResource, Show } from 'solid-js'
+import { Component, createResource, Match, Switch } from "solid-js";
 import {
   createInitDataSignal,
   createUserSignal,
   MainButton,
-} from 'telegram-webapp-solid'
-import { retreive } from 'shared'
-import { Layout } from './Layout'
+} from "telegram-webapp-solid";
+import { retreive } from "shared";
+import { Layout } from "./Layout";
 
 const App: Component = () => {
-  const user = createUserSignal()
-  const [initData, sendData] = createInitDataSignal()
+  const user = createUserSignal();
+  const [initData, sendData] = createInitDataSignal();
 
   const [data] = createResource(() => {
-    return retreive(user?.id)
-  })
+    return retreive(user?.id);
+  });
 
   return (
     <Layout>
       <div class="my-auto flex flex-col">
         <p class="text-center text-hint w-full">
-          Hi {user?.first_name ?? ''}, welcome to the demo app. <br />
+          Hi {user?.first_name ?? ""}, welcome to the demo app. <br />
         </p>
-        <Show
-          when={!data.loading}
-          fallback={
+        <Switch>
+          <Match when={data.loading}>
             <span class="text-center text-hint w-full">Loading your data</span>
-          }
-        >
-          <pre>
-            <code>{JSON.stringify(data(), null, 2)}</code>
-          </pre>
-        </Show>
+          </Match>
+          <Match when={data.error}>
+            <span class="text-center text-error w-full">
+              Error loading your data
+            </span>{" "}
+            <br />
+            <pre class="text-error mx-auto">
+              <code>{JSON.stringify(data.error, null, 2)}</code>
+            </pre>
+          </Match>
+          <Match when={data()}>
+            <pre class="text-hint mx-auto">
+              <code>{JSON.stringify(data(), null, 2)}</code>
+            </pre>
+          </Match>
+        </Switch>
       </div>
 
       <MainButton
         text="Close the app"
         onClick={() => {
-          sendData(JSON.stringify({ date: new Date() }))
+          sendData(JSON.stringify({ date: new Date() }));
         }}
       />
     </Layout>
-  )
-}
+  );
+};
 
-export default App
+export default App;
