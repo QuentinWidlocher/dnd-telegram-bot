058a077c1e7abcfe5c0a25aa0c3c30fb5bfad652
QuentinWidlocher <qwidlocher@gmail.com>
2022-07-14 14:00:32 +0200

    🐛  Fix search

--- shared/src/spells.ts
+++ shared/src/spells.ts
@@ -5,16 +5,15 @@ export function searchSpellByName(name: string): Spell[] {
   const fullRegexString = name
     .split(" ")
     .map((s) => s.trim())
-    .map(fuzzySearchRegexTemplate)
-    .join("");
+    .join(".*");
 
   const regex = new RegExp(fullRegexString, "i");
   console.log("regex", regex);
   return spells.filter((s) => (s.name?.match(regex) ?? []).length > 0) ?? [];
 }
 
-export function fuzzySearchRegexTemplate(word: string) {
-  return `(?=[a-z\u00E0-\u00FC]*${word}[a-z\u00E0-\u00FC]*)`;
+export function fuzzySearchRegexTemplate(words: string[]) {
+  return `(?=[a-z\u00E0-\u00FC]*(${words.join('|')})[a-z\u00E0-\u00FC]*)`;
 }
 
 export const schoolsNames: {
